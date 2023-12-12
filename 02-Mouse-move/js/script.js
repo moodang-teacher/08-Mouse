ecb22bae16e13615c21d7d0015b1bb30e939b000
moodang <yssmi@naver.com>
2023-12-12 16:17:06 +0900

    스크롤에 따른 동작 멈추기

--- 02-Mouse-move/js/script.js
+++ 02-Mouse-move/js/script.js
@@ -70,12 +70,8 @@ $(function () {
     console.log(scrollTop, targetPos);
 
     // 만약 스크롤값이 targetPos를 넘어가면
-    if (scrollTop >= targetPos) {
-      // 애니메이션을 멈추고
-      cancelAnimationFrame(movingObj);
-    } else {
-      // 그렇지 않으면 다시 애니메이션 실행
-      initMoving();
-    }
+    if (scrollTop >= targetPos) cancelAnimationFrame(movingObj);
+
+    if (scrollTop === 0) initMoving();
   });
 });
